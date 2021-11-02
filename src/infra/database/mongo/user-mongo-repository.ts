37370c62a9d27eb4c2628377feea510e46312f5a
Elisandro <elisandro@ulbra.edu.br>
2021-11-01 22:46:46 -0300

    feat: add repository UserMongoRepository

---
+++ src/infra/database/mongo/user-mongo-repository.ts
@@ -0,0 +1,22 @@
+
+import { MongoHelper } from '@/infra/database/mongo'
+import {
+  SearchUserRepositoryContract
+} from '@/application/contracts/database/user'
+
+export class UserMongoRepository implements
+SearchUserRepositoryContract {
+  async load (params: SearchUserRepositoryContract.Params): Promise<SearchUserRepositoryContract.Result> {
+    const { name } = params
+    const badgeCollection = await MongoHelper.getCollection('users', 'liuv')
+    const collection = await badgeCollection.findOne({ name: name })
+
+    if (collection) {
+      const user = MongoHelper.map(collection)
+      const userId: string = user?.id
+
+      return { userId: userId }
+    }
+    return undefined
+  }
+}
