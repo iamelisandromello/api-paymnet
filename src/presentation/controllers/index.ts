d530acc60d82a47b084bd2db4334310bb1e191f0
Elisandro <elisandro@ulbra.edu.br>
2021-11-01 23:03:01 -0300

    feat: add controller TransactionController

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './payment-controller'
 export * from './encrypted-body'
+export * from './transaction-controller'
---
+++ src/presentation/controllers/transaction-controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from '@/presentation/interfaces'
+import { serverError, success, brokerError, badRequest } from '@/presentation/helpers/http'
+import { BrokerError, InvalidParamError } from '@/presentation/errors'
+import { CheckBin } from '@/domain/usecases/card'
+import { LoadUser } from '@/domain/usecases/user'
+import { LoadDataWallet } from '@/domain/usecases/wallet'
+import { PublishPaymentRequest } from '@/domain/usecases/publications'
+
+export class TransactionController implements Controller {
+  constructor (
+    private readonly checkBin: CheckBin,
+    private readonly loadUser: LoadUser,
+    private readonly loadDataWallet: LoadDataWallet,
+    private readonly publishPaymentRequest: PublishPaymentRequest
+  ) {}
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const scheme = await this.checkBin.check(request.body.cardNumber)
+      if (!scheme) return badRequest(new InvalidParamError('Number Card'))
+
+      Object.assign(request.body, {
+        scheme: scheme?.scheme ?? undefined,
+        schemeId: scheme?.schemeId ?? undefined
+      })
+
+      console.log('Controller Transaction: ', request.body)
+      const { name, schemeId } = request.body
+
+      const isUserId = await this.loadUser.search({ name })
+
+      if (isUserId) {
+        const wallet = await this.loadDataWallet.search({ userId: isUserId.userId, schemeId })
+        console.log('Service WALLET: ', wallet)
+
+        Object.assign(wallet, { valor: '30,00' })
+
+        if (wallet) {
+          const token = await this.publishPaymentRequest.publish(this.preparePlubishParams(wallet))
+          if (!token) return brokerError(new BrokerError())
+        }
+      }
+
+      const token = 'JKALAM67575757575'
+
+      return success(token)
+    } catch (error: any) {
+      return serverError(error)
+    }
+  }
+
+  private preparePlubishParams (
+    params: Record<string, any>
+  ): PublishPaymentRequest.Params {
+    return {
+      tokenCard: params.tokenCard,
+      valor: params.valor,
+      paymentProviderId: params.paymentProviderId
+    }
+  }
+}
