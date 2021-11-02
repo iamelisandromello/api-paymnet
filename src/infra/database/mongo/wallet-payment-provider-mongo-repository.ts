5ffcd53461ff2e5eb333d5e7e09fa5c4ebd31d3e
Elisandro <elisandro@ulbra.edu.br>
2021-11-01 22:47:53 -0300

    feat: add repository WalletPaymentProviderMongoRepository

---
+++ src/infra/database/mongo/wallet-payment-provider-mongo-repository.ts
@@ -0,0 +1,44 @@
+import { MongoHelper, QueryBuilder } from '@/infra/database/mongo'
+import { LoadWalletForPaymentRepositoryContract } from '@/application/contracts/database/wallet'
+import { walletPaymentProviderTransformer } from '@/infra/database/mongo/transformers'
+
+export class WalletPaymentProviderMongoRepository implements LoadWalletForPaymentRepositoryContract {
+  async load (
+    params: LoadWalletForPaymentRepositoryContract.Params
+  ): Promise<LoadWalletForPaymentRepositoryContract.Result> {
+    const { userId, schemeId } = params
+
+    const badgeCollection = await MongoHelper.getCollection('wallet', 'liuv')
+
+    const query = new QueryBuilder()
+      .match({
+        user_id: userId,
+        scheme_id: schemeId
+      })
+      .project({
+        _id: 1
+      })
+      .lookup({
+        from: 'wallet_payment_providers',
+        localField: '_id',
+        foreignField: 'wallet_id',
+        as: 'wallet'
+      })
+      .project({
+        _id: 0,
+        wallet: {
+          payment_provider_id: 1,
+          token: 1
+        }
+      })
+      .unwind({
+        path: '$wallet'
+      })
+      .build()
+
+    const aggregateResult = await badgeCollection.aggregate(query).toArray()
+
+    const teste = walletPaymentProviderTransformer.map(aggregateResult)
+    return teste ?? undefined
+  }
+}
